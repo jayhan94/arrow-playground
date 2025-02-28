60ccc87212126c90aca23b71ed775e46f1fbe7c6
Jay Han <feynmanhan94@gmail.com>
2025-02-28 21:19:36 +0800

    only compare stream cost

--- src/main.rs
+++ src/main.rs
@@ -15,19 +15,19 @@ pub const DATA_COUNT: i32 = 10000000;
 
 #[allow(unused_variables)]
 fn main() {
-    let start_time = std::time::Instant::now();
     let source = TraditionalDataSource::new();
     let filter = TraditionalFilter::new(Box::new(source));
     let project = TraditionalProject::new(Box::new(filter));
     let mut stream = project.execute();
+    let start_time = std::time::Instant::now();
     while let Some(value) = stream.deref_mut().poll_next() {}
     println!("traditional: cost {} ms", start_time.elapsed().as_millis());
 
-    let start_time = std::time::Instant::now();
     let source = ColumnarDataSource::new();
     let filter = ColumnarFilter::new(Box::new(source));
     let project = ColumnarProject::new(Box::new(filter));
     let mut stream = project.execute();
+    let start_time = std::time::Instant::now();
     while let Some(value) = stream.deref_mut().poll_next() {}
     println!("columnar: cost {} ms", start_time.elapsed().as_millis());
 }
